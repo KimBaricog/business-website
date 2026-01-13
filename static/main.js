1121d647b02ae0915b0cbddd2419d507e5536eca
Kim <kimbaricog4@gmail.com>
2026-01-13 12:28:28 +0800

    M

--- static/main.js
+++ static/main.js
@@ -10,6 +10,7 @@ const img = document.getElementById("img");
 swith.addEventListener("change", () => {
   document.body.classList.toggle("dark", swith.checked);
   if (swith.checked) {
+    img.style.transition = "background-color 0.3s ease, color 0.3s ease;";
     img.src = "static/whitelogo.png";
   } else {
     img.src = "static/logo.png";
